29fe5be9d205b0556855f8e8e1e8b0615082f83e
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-05-21 14:38:55 +0200

    Struktur angelegt

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<head>
+	<meta charset="utf-8">
+	<title>The Awesome "Mach Dein Spiel" Editor</title>
+
+
+</head>
+<body>
+	<header id="header" class="">
+		
+	</header><!-- /header -->
+	<main>
+		
+	</main>
+	<footer>
+
+	</footer>
+</body>
