8c0703ebb62e547a257016740277fb56e31b5347
nhatje <nhatje@gmx.de>
2014-05-28 15:57:15 +0200

    Css ist drin

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
 
 	<!-- JQUERY IMPORT -->
 	<link rel="stylesheet" href="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.4/themes/smoothness/jquery-ui.css" />
+	<link rel="stylesheet" href="frontCss/frontpage.css" />
 
 </head>
 <body>
