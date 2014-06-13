3b13b4455c127d0d5522f096d9ff92f20ad7355b
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-06-13 12:34:27 +0200

    drag and drop mit revert wenn nicht auf droppable fläche

--- js/editor/application/StateManager.js
+++ js/editor/application/StateManager.js
@@ -1,3 +1,17 @@
 function StateManager() {
 
+	this.states = [];
+
+
+
+	StateManager.prototype.addState = function() {
+		var stateId = this.states.length + 1;
+
+
+
+		// <div class="state" data-id="1" style="top: 100px; left: 200px;">
+		// 	<div class="state-title" >Move Around</div>
+		// </div>
+	}
+
 }
--- js/editor/dragndrop.js
+++ js/editor/dragndrop.js
@@ -9,21 +9,31 @@ var stateID;
 $( ".state" ).draggable({
 	cursor: "move",
 	stack: "#editor-divs div",
+	distance: 10,
+	revert: "invalid",
     start: function() {
     	startX = parseInt(this.style.left);
     	startY = parseInt(this.style.top);	
     	stateID = this.getAttribute('data-id');
     },
     drag: function() {
-
+    	deltaX = parseInt(this.style.left) - startX;
+    	deltaY = parseInt(this.style.top) - startY;
+		console.log('deltaX: ' +deltaX);
+		console.log('deltaY: ' +deltaY);
 	},
     stop: function() {
     	
     	
     	deltaX = parseInt(this.style.left) - startX;
     	deltaY = parseInt(this.style.top) - startY;
-		console.log('deltaX: ' +deltaX);
-		console.log('deltaY: ' +deltaY);
+    }
+});
+
+
+ $( "#editor-divs" ).droppable({	
+    drop: function( event, ui ) {
+    	console.log('dropped');
     }
 });
 
