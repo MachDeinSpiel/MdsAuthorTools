def604a791d3d04a3266f9ca02c83c512ec16011
JGerdes <jonas.gerdes@gmx.net>
2014-07-03 21:05:18 +0200

    JSON vom RAW-Editor können hochgeladen werden

--- js/editor/application/raweditor.js
+++ js/editor/application/raweditor.js
@@ -12,6 +12,10 @@ RAWEDITOR.init = function(){
 		e.stopPropagation();
 	});
 
+	$('#raw-panel input[type=button]').on('click', function(){
+		RAWEDITOR.upload();
+	});
+
 	this.clientEditor = ace.edit("editor-client");
     this.clientEditor.setTheme("ace/theme/xcode");
     this.clientEditor.getSession().setMode("ace/mode/json");
@@ -30,4 +34,31 @@ RAWEDITOR.upload = function(){
 	console.log("upload");
 	console.log(this.clientEditor.getValue());
 	console.log(this.serverEditor.getValue());
+	var config = exporter.generateConfig(true);
+	if(config == false){
+		console.error('Error while generating config JSON');
+		return;
+	}
+	var serverJSON = this.serverEditor.getValue();
+	var clientJSON = this.clientEditor.getValue();
+	var name = document.querySelector('input[name=config-game-name]').value;
+	var url = "http://mds.informatik.hs-bremen.de:1380/authortools/upload.php?t="+(new Date()).getTime();
+	var scope = exporter;
+	$.post(url, 'gamename='+name+'&json='+serverJSON, function(serverURL){
+		console.log("ServerURL:"+serverURL);
+		$.post(url, 'gamename='+name+'&json='+clientJSON, function(clientURL){
+			console.log("ClientURL:"+clientURL);
+			showLogin(function(name, pw){
+				console.log(name, pw);
+				config.serverurl = serverURL;
+				config.clienturl = clientURL;
+				config.author = name;
+				console.log(config);
+				var data = '{"mode":"config","username":'+name+', "password":'+pw+',"config":'+JSON.stringify(config)+'}';
+				console.log("data:",data);
+				scope.websocket.send(data);
+			});
+		});
+	});
 }
+
