09dc77c75d7edfb08e7a64fd845abfd8e281c08d
Jonathan Wiemers <jwiemers@webtwoart.com>
2014-07-03 19:47:46 +0200

    ace editor

--- js/editor/application/raweditor.js
+++ js/editor/application/raweditor.js
@@ -1,5 +1,8 @@
 var RAWEDITOR = {};
 
+RAWEDITOR.clientEditor = undefined;
+RAWEDITOR.serverEditor = undefined;
+
 RAWEDITOR.init = function(){
 	$('#raw-wrapper').on('click', function(){
 		$(this).css('display', 'none');
@@ -9,9 +12,22 @@ RAWEDITOR.init = function(){
 		e.stopPropagation();
 	});
 
+	this.clientEditor = ace.edit("editor-client");
+    this.clientEditor.setTheme("ace/theme/xcode");
+    this.clientEditor.getSession().setMode("ace/mode/json");
+
+	this.serverEditor = ace.edit("editor-server");
+	this.serverEditor.setTheme("ace/theme/xcode");
+    this.serverEditor.getSession().setMode("ace/mode/json");
 }
 
 RAWEDITOR.show = function(){
 	$('#raw-wrapper').css('display', 'block');
+}
 
+
+RAWEDITOR.upload = function(){
+	console.log("upload");
+	console.log(this.clientEditor.getValue());
+	console.log(this.serverEditor.getValue());
 }
